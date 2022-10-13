f5220120c2269ee887d97e2dc6796c4f6fb8d516
parkhayoung615 <parkhayoung615@gmail.com>
2022-10-13 14:49:54 +0900

    Update Project

---
+++ 221013/H20104/WebContent/index.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<div>
+	<a href="/bs">1. BookServlet 실행 : DTO(VO), DB 내부에서 구현</a>
+	<br><br>
+	
+	<a href="/bs2">2. BookServlet2 실행 : DTO(VO), DB DAO2로 분리 구현</a>
+</div>
+</body>
+</html>
--- 221013/H20104/src/biz/BookServlet.java
+++ 221013/H20104/src/biz/BookServlet.java
@@ -18,7 +18,7 @@ import dao.BookDAO;
 import dto.BookDTO;
 
 
-@WebServlet("/bs2")
+@WebServlet("/bs")
 public class BookServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
       
@@ -40,45 +40,9 @@ public class BookServlet extends HttpServlet {
 			String bookName = dto.getBookName();
 			String bookLoc = dto.getBookLoc();
 			
-			out.println(bookId + ", " + bookName + ", " + bookLoc + "<br>");
+			out.print(bookId + ", " + bookName + ", " + bookLoc + "<br>");
 		}
 		
-		/*
-		String driver = "oracle.jdbc.driver.OracleDriver";
-		String url = "jdbc:oracle:thin:@localhost:1521:orcl";
-		String id = "systme";
-		String pwd = "1234";
-		
-		Connection conn = null;
-		Statement stmt = null;
-		ResultSet rs = null;
-		
-		try {
-			Class.forName(driver);
-			conn = DriverManager.getConnection(url, id, pwd);
-			stmt = conn.createStatement();
-			String sql = "SELECT * FROM BOOK";
-			rs = stmt.executeQuery(sql);
-			
-			while(rs.next()) {
-				int bookId = rs.getInt("book_id");
-				String bookName = rs.getString("book_name");
-				String bookLoc = rs.getString("book_loc");
-				
-				out.println(bookId + ", " + bookName +", " + "bookLoc" + "<br>");
-				
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if (rs != null) rs.close();
-				if (stmt != null) stmt.close();
-				if (conn != null) conn.close();
-			} catch (Exception e2) {
-				e2.printStackTrace();
-			}
-		} */
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
